8fa8ddaf932d1023bbd77427f348b5ce8dfd84c2
Phil Ross <phil.ross@gmail.com>
2016-04-18 22:33:27 +0100

    Update to tzdata version 2016d.
    https://mm.icann.org/pipermail/tz-announce/2016-April/000038.html

--- lib/tzinfo/data/version.rb
+++ lib/tzinfo/data/version.rb
@@ -1,7 +1,7 @@
 module TZInfo
   module Data
     # TZInfo::Data version number.
-    VERSION = '1.2016.3'
+    VERSION = '1.2016.4'
   
     # TZInfo::Data version information.
     module Version
@@ -14,7 +14,7 @@ module TZInfo
       
       # The version of the {IANA Time Zone Database}[http://www.iana.org/time-zones]
       # used to generate this version of TZInfo::Data.
-      TZDATA = '2016c'
+      TZDATA = '2016d'
     end
   end
 end
