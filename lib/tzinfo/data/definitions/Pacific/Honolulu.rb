6b6f51cf36d0fdb1057489ca3fbdb3c818699aa1
Phil Ross <phil.ross@gmail.com>
2018-10-27 18:44:47 +0100

    Update to tzdata version 2018g.
    https://mm.icann.org/pipermail/tz-announce/2018-October/000052.html

--- lib/tzinfo/data/definitions/Pacific/Honolulu.rb
+++ lib/tzinfo/data/definitions/Pacific/Honolulu.rb
@@ -14,14 +14,17 @@ module TZInfo
             tz.offset :o0, -37886, 0, :LMT
             tz.offset :o1, -37800, 0, :HST
             tz.offset :o2, -37800, 3600, :HDT
-            tz.offset :o3, -36000, 0, :HST
+            tz.offset :o3, -37800, 3600, :HWT
+            tz.offset :o4, -37800, 3600, :HPT
+            tz.offset :o5, -36000, 0, :HST
             
             tz.transition 1896, 1, :o1, -2334101314, 104266329343, 43200
             tz.transition 1933, 4, :o2, -1157283000, 116505265, 48
             tz.transition 1933, 5, :o1, -1155436200, 116506291, 48
-            tz.transition 1942, 2, :o2, -880198200, 116659201, 48
+            tz.transition 1942, 2, :o3, -880198200, 116659201, 48
+            tz.transition 1945, 8, :o4, -769395600, 58360379, 24
             tz.transition 1945, 9, :o1, -765376200, 116722991, 48
-            tz.transition 1947, 6, :o3, -712150200, 116752561, 48
+            tz.transition 1947, 6, :o5, -712150200, 116752561, 48
           end
         end
       end
