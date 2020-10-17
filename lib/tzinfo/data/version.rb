cf88795eb19aa2b08d6bdd3f330d250a0316dda9
Phil Ross <phil.ross@gmail.com>
2020-10-17 12:39:40 +0100

    Update to tzdata version 2020c.
    https://mm.icann.org/pipermail/tz-announce/2020-October/000060.html

--- lib/tzinfo/data/version.rb
+++ lib/tzinfo/data/version.rb
@@ -1,7 +1,7 @@
 module TZInfo
   module Data
     # TZInfo::Data version number.
-    VERSION = '1.2020.2'
+    VERSION = '1.2020.3'
 
     # TZInfo::Data version information.
     module Version
@@ -14,7 +14,7 @@ module TZInfo
 
       # The version of the {IANA Time Zone Database}[https://www.iana.org/time-zones]
       # used to generate this version of TZInfo::Data.
-      TZDATA = '2020b'
+      TZDATA = '2020c'
     end
   end
 end
