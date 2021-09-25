bd1a1ea2357997cc03dc1b52c0f7a7d464ea6807
Phil Ross <phil.ross@gmail.com>
2021-09-25 20:26:04 +0100

    Support zdump 2021b output in definition tests.
    zdump 2021b includes negative years, UTC instances specified as Unix
    timestamps and UTC dates in the far future exceeding the given cutoff
    year (-c). For example:
    
    Australia/Melbourne  -67768040609775592 = Thu Jan  1 00:00:00 -2147481748 LMT isdst=0 gmtoff=34792
    Australia/Melbourne  -67768040609740801 = Thu Jan  1 09:39:51 -2147481748 LMT isdst=0 gmtoff=34792
    Australia/Melbourne  Thu Jan  1 00:00:00 -2147481748 UT = Thu Jan  1 09:39:52 -2147481748 LMT isdst=0 gmtoff=34792
    ...
    Australia/Melbourne  Wed Dec 31 12:59:59 2147485547 UT = Wed Dec 31 23:59:59 2147485547 AEDT isdst=1 gmtoff=39600

--- test/tc_definitions.rb
+++ test/tc_definitions.rb
@@ -1,8 +1,13 @@
 require File.join(File.expand_path(File.dirname(__FILE__)), 'test_utils')
 require 'date'
 require 'tmpdir'
+require 'set'
 
 class TCDefinitions < Minitest::Test
+  @@warned_for_datetime = Set.new
+  @@warned_for_time = Set.new
+  @@warned_for_datetime_lock = Mutex.new
+  @@warned_for_time_lock = Mutex.new
 
   class << self
     private
@@ -54,8 +59,8 @@ class TCDefinitions < Minitest::Test
         IO.popen(zdump_cmdline) do |io|
           io.each_line do |line|
             line.chomp!
-            check_zdump_line(zone, line) {|s| parse_as_datetime(s)}
-            check_zdump_line(zone, line) {|s| parse_as_time(s)}
+            check_zdump_line(zone, line, max_year) {|s| parse_as_datetime(s) }
+            check_zdump_line(zone, line, max_year) {|s| parse_as_time(s) }
           end
         end
 
@@ -107,19 +112,38 @@ class TCDefinitions < Minitest::Test
     end
   end
 
-  def check_zdump_line(zone, line)
-    if line =~ /\A[^\s]+\s+([A-Za-z][a-z]{2}\s[A-Za-z][a-z]{2}\s+[0-9]+\s[0-9]+:[0-9]+:[0-9]+\s[0-9]+)\s[A-Z]+\s=\s([A-Za-z][a-z]{2}\s[A-Za-z][a-z]{2}\s+[0-9]+\s[0-9]+:[0-9]+:[0-9]+\s[0-9]+)\s([A-Za-z0-9+\-]+)\sisdst=([01])/ then
-      assert_nothing_raised("Exception raised processing line:\n#{line}") do
-        utc = yield $1
-        local = yield $2
-        identifier = $3
-        is_dst = $4 == '1'
+  def fix_datetime_start(time_or_datetime)
+    # Handle TZInfo 2.0.0 to 2.0.4 returning DateTime instances that interpret
+    # timestamps using the Julian calendar when they are before the adoption of
+    # the Gregorian calendar in Italy. Adjust the start so the date and time
+    # can be intepreted using the Gregorian calendar (as would be the case with
+    # the Time class).
+    if time_or_datetime.respond_to?(:start) && time_or_datetime.start != Date::GREGORIAN && time_or_datetime.respond_to?(:new_start)
+      time_or_datetime.new_start(Date::GREGORIAN)
+    else
+      time_or_datetime
+    end
+  end
 
-        if utc && local
+  def check_zdump_line(zone, line, max_year)
+    if line =~ /\A[^\s]+\s+(?:([A-Za-z][a-z]{2}\s[A-Za-z][a-z]{2}\s+[0-9]+\s[0-9]+:[0-9]+:[0-9]+\s-?[0-9]+)\s[A-Z]+|(-?[0-9]+))\s=\s([A-Za-z][a-z]{2}\s[A-Za-z][a-z]{2}\s+[0-9]+\s[0-9]+:[0-9]+:[0-9]+\s-?[0-9]+)\s([A-Za-z0-9+\-]+)\sisdst=([01])/ then
+      assert_nothing_raised("Exception raised processing line:\n#{line}") do
+        utc = yield $1 || $2
+        local = yield $3
+        identifier = $4
+        is_dst = $5 == '1'
+
+        # zdump 2021b outputs dates with years in the far future (after
+        # max_year). TZInfo::Data doesn't include a rule for years beyond the
+        # end of the data. Ignore them. Note that the start of max_year is the
+        # cutoff passed to zdump.
+        if utc && local && utc.year < max_year
           tzi_local = zone.utc_to_local(utc)
           tzi_period = zone.period_for_utc(utc)
           tzi_identifier = tzi_period.zone_identifier.to_s
           tzi_is_dst = tzi_period.dst?
+
+          tzi_local = fix_datetime_start(tzi_local)
 
           # TZInfo v2 returns times with a local offset, TZInfo v1 doesn't. For
           # now, compare just the date and time, by removing the offset.
@@ -143,6 +167,8 @@ class TCDefinitions < Minitest::Test
           tzi_utc = zone.local_to_utc(local, is_dst) {|periods| resolve_ambiguity(periods, identifier, tzi_period)}
           tzi_local_identifier = (zone.period_for_local(local, is_dst) {|periods| resolve_ambiguity(periods, identifier, tzi_period)}).zone_identifier.to_s
 
+          tzi_utc = fix_datetime_start(tzi_utc)
+
           msg_zdump  = "zdump:         #{zone.identifier} #{utc} UTC #{local} #{identifier}"
           msg_tzinfo = "tzinfo (l->u): #{zone.identifier} #{tzi_utc} UTC #{local} #{tzi_local_identifier}"
           message = "#{msg_zdump}\n#{msg_tzinfo}"
@@ -156,26 +182,47 @@ class TCDefinitions < Minitest::Test
     end
   end
 
+  MONTHS = %w(Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec).freeze
+
   def parse_zdump_timestamp(s)
-    if s =~ /\A(?:Mon|Tue|Wed|Thu|Fri|Sat|Sun)\s(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s+(\d\d?)\s(\d\d):(\d\d):(\d\d)\s(\d+)\z/
-      month = %w(Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec).index($1) + 1
-      [$6.to_i, month, $2.to_i, $3.to_i, $4.to_i, $5.to_i]
+    if s =~ /\A(?:Mon|Tue|Wed|Thu|Fri|Sat|Sun)\s(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s+(\d\d?)\s(\d\d):(\d\d):(\d\d)\s(-?\d+)\z/
+      month = MONTHS.index($1) + 1
+      [[$6.to_i, month, $2.to_i, $3.to_i, $4.to_i, $5.to_i], nil]
+    elsif s =~ /\A-?[0-9]+\z/
+      [nil, s.to_i]
     else
       raise "Unexpected zdump timestamp format: #{s}"
     end
   end
 
+  JD_EPOCH = 2440588
+
   def parse_as_datetime(s)
-    year, month, day, hour, minute, second = parse_zdump_timestamp(s)
-    DateTime.new(year, month, day, hour, minute, second)
+    datetime_params, timestamp = parse_zdump_timestamp(s)
+    begin
+      datetime_params ?
+        DateTime.new(*datetime_params, 0, Date::GREGORIAN) :
+        DateTime.jd(JD_EPOCH + Rational(timestamp, 86400)).new_start(Date::GREGORIAN)
+    rescue => e
+      # JRuby doesn't support DateTimes far in the past.
+      type = e.to_s
+      if @@warned_for_datetime_lock.synchronize { @@warned_for_datetime.add?(type) }
+        warn("Skipping DateTime test for #{s}: #{type}")
+      end
+      nil
+    end
   end
 
   def parse_as_time(s)
-    year, month, day, hour, minute, second = parse_zdump_timestamp(s)
+    time_params, timestamp = parse_zdump_timestamp(s)
     begin
-      Time.utc(year, month, day, hour, minute, second)
-    rescue
+      time_params ? Time.utc(*time_params) : Time.at(timestamp).utc
+    rescue => e
       # Time doesn't support the full range required on all platforms.
+      type = e.to_s
+      if @@warned_for_time_lock.synchronize { @@warned_for_time.add?(type) }
+        warn("Skipping Time test for #{s}: #{type}")
+      end
       nil
     end
   end
