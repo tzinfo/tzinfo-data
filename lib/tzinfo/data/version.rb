8e6a2234c24e2067631cad7f765a2285aa462f35
Phil Ross <phil.ross@gmail.com>
2023-03-24 22:35:29 +0000

    Update to tzdata version 2023b.
    https://mm.icann.org/pipermail/tz-announce/2023-March/000078.html

--- lib/tzinfo/data/version.rb
+++ lib/tzinfo/data/version.rb
@@ -1,7 +1,7 @@
 module TZInfo
   module Data
     # TZInfo::Data version number.
-    VERSION = '1.2023.1'
+    VERSION = '1.2023.2'
 
     # TZInfo::Data version information.
     module Version
@@ -14,7 +14,7 @@ module TZInfo
 
       # The version of the {IANA Time Zone Database}[https://www.iana.org/time-zones]
       # used to generate this version of TZInfo::Data.
-      TZDATA = '2023a'
+      TZDATA = '2023b'
     end
   end
 end
