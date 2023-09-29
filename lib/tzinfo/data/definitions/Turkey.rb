5ec57b9131abc0979cba44c91b78a838fb1deb86
James Dennes <jdennes@gmail.com>
2023-09-29 17:10:15 +0200

    Undo unnecessary change

--- lib/tzinfo/data/definitions/Turkey.rb
+++ lib/tzinfo/data/definitions/Turkey.rb
@@ -9,7 +9,7 @@ module TZInfo
       module Turkey
         include TimezoneDefinition
         
-        linked_timezone 'Türkiye', 'Europe/Istanbul'
+        linked_timezone 'Turkey', 'Europe/Istanbul'
       end
     end
   end
